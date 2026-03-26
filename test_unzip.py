e6e3c9e8705e05b20254f490ca436f24bcd5cb83
João Machado <jv.muca@gmail.com>
2026-03-25 22:09:49 -0300

    n8n data setup v1

---
+++ test_unzip.py
@@ -0,0 +1,16 @@
+import zipfile
+import os
+
+
+def zip_files(list_of_files: list[str]) -> str:
+    # Create the ZIP file and write files into it
+    with zipfile.ZipFile("output.zip", mode="w") as zipf:
+        for file in list_of_files:
+            zipf.write(file)
+
+def unzip_files(zip_file: str, extraction_path: str) -> list[str]:
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extraction_path)
+
+zip_files(["data/clientes.csv", "data/contratos.csv", "data/leituras.csv"])
+unzip_files("output.zip", "data_output")
